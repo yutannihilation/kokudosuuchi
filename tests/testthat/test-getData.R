50aa80bdc58813ff18e7614e29267c98e3b5679a
Hiroaki Yutani <yutani.ini@gmail.com>
2016-11-13 08:14:30 +0900

    suppress warnings

--- tests/testthat/test-getData.R
+++ tests/testthat/test-getData.R
@@ -1,6 +1,10 @@
 context("getData")
 
 test_that("getKSJData works", {
+  if (!identical(Sys.getenv("TRAVIS"), "true")) {
+    skip("RUN only on Travis")
+  }
+
   d <- getKSJData("http://nlftp.mlit.go.jp/ksj/gml/data/W05/W05-07/W05-07_03_GML.zip")
 
   expect_equal(names(d), c("W05-07_03-g_RiverNode", "W05-07_03-g_Stream"))
@@ -16,7 +20,7 @@ test_that("getKSJData randomly works", {
 
   ksj_summary <- getKSJSummary()
   ksj_urls <- getKSJURL(identifier = sample(ksj_summary$identifier, 1))
-  d <- getKSJData(sample(ksj_urls$zipFileUrl, 1))
+  suppressWarnings(d <- getKSJData(sample(ksj_urls$zipFileUrl, 1)))
 
   expect_true(all(purrr::map_lgl(d, ~ class(.) %in% c("SpatialPointsDataFrame",
                                                       "SpatialLinesDataFrame",

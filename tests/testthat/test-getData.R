4fd0988a9eb994c33aa7ced4cb4c94b347aac00d
Hiroaki Yutani <yutani.ini@gmail.com>
2016-11-13 09:05:20 +0900

    remove random test as it always broken

--- tests/testthat/test-getData.R
+++ tests/testthat/test-getData.R
@@ -8,26 +8,3 @@ test_that("getKSJData works", {
   expect_true(inherits(d$`L01-01_36-g_LandPrice`, "SpatialPointsDataFrame"))
   expect_true(inherits(d$`L01-01_36-g_LandPrice`@data, "data.frame"))
 })
-
-
-test_that("getKSJData randomly works", {
-  if (!identical(Sys.getenv("TRAVIS"), "true")) {
-    skip("RUN only on Travis")
-  }
-
-  ksj_summary <- getKSJSummary()
-  ksj_urls <- getKSJURL(identifier = sample(ksj_summary$identifier, 1))
-  with_mock(
-    warn_no_corresp_names = function(...) TRUE,
-    {
-      url <- sample(ksj_urls$zipFileUrl, 1)
-      cat(url)
-      d <- getKSJData(url)
-    }
-  )
-
-  expect_true(all(purrr::map_lgl(d, ~ class(.) %in% c("SpatialPointsDataFrame",
-                                                      "SpatialLinesDataFrame",
-                                                      "SpatialPolygonsDataFrame"))))
-  expect_true(class(d[[1]]@data), "data.frame")
-})

1e4c00b057668e0626aa28795be9b58f31794553
Hiroaki Yutani <yutani.ini@gmail.com>
2017-08-29 22:02:02 +0900

    update KSJCodeDescriptionURL

--- scripts/update_KSJCodeDescriptionURL.R
+++ scripts/update_KSJCodeDescriptionURL.R
@@ -1,24 +1,22 @@
 library(rvest)
 library(dplyr, warn.conflicts = FALSE)
 
-x <- read_html("http://nlftp.mlit.go.jp/ksj/index.html")
+page <- read_html("http://nlftp.mlit.go.jp/ksj/index.html")
 
-d <- x %>%
-  html_nodes(css = "td > a") %>%
-  {
-    tibble::tibble(url  = html_attr(., "href"),
-                   name = html_text(.))
-  }
+a_nodes <- page %>%
+  html_nodes(css = "td > a")
+
+d <- tibble::tibble(url  = html_attr(a_nodes, "href"),
+                    name = html_text(a_nodes))
 d
 
-
 KSJCodeDescriptionURL <- d %>%
-  filter(startsWith(url, "gml/datalist")) %>%
-  mutate(code = stringi::stri_extract_first_regex(url, "[A-Z][0-9]+"),
+  mutate(code = stringi::stri_extract_first_regex(url, "(?<=KsjTmplt-).*?(?=(-v\\d+_\\d+)?\\.html)"),
          url  = glue::glue('http://nlftp.mlit.go.jp/ksj/{url}')) %>%
+  filter(!is.na(code)) %>%
   arrange(code)
 
-KSJCodeDescriptionURL
+glimpse(KSJCodeDescriptionURL)
 
 readr::write_csv(KSJCodeDescriptionURL, "data-raw/KSJCodeDescriptionURL.csv")
 devtools::use_data(KSJCodeDescriptionURL, overwrite = TRUE)

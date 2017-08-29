15f5350fc421c1bafa4af1c7beaa86afd2631304
Hiroaki Yutani <yutani.ini@gmail.com>
2017-08-29 23:22:11 +0900

    download htmls

--- scripts/update_KSJCodeTranslations.R
+++ scripts/update_KSJCodeTranslations.R
@@ -12,10 +12,16 @@ download_safely <- purrr::safely(function(url) {
   return(TRUE)
 })
 
-purrr::map(KSJIdentifierDescriptionURL$url[1:3],
-           download_safely)
+result <- purrr::map(KSJIdentifierDescriptionURL$url,
+                     download_safely)
 
-x <- read_html("http://nlftp.mlit.go.jp/ksj/gml/datalist/KsjTmplt-W07.html")
+if (any(purrr::map_lgl(result, ~ !is.null(.$error)))) {
+  stop("some error happened")
+}
+
+htmls <- file.path("downloaded_html", paste0("datalist-", basename(KSJIdentifierDescriptionURL$url)))
+
+x <- read_html(htmls[1])
 x %>%
   html_nodes(css = "a") %>%
   html_attr("href") %>%
